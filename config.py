0791eba2c231f11219caab5bce6d854051c3090c
Anthony10700 <anthony.thillerot@gmail.com>
2020-11-14 12:16:50 +0100

    heroku

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ CONSTANT of flask and API
 """
 TEMPLATE_DIR = os.path.abspath('GrandPyBotApp/templates')
 STATIC_DIR = os.path.abspath('GrandPyBotApp/static')
-KEY_MAPS_PLACES = "YOUR_KEY_API"
+KEY_MAPS_PLACES = os.environ.get('MAPS_KEY')
 

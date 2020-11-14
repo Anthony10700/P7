0b6821ff63b78d0fad07e587a2e3d8e1aee2421b
Anthony10700 <anthony.thillerot@gmail.com>
2020-11-14 10:36:17 +0100

    modif gitignore

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ CONSTANT of flask and API
 """
 TEMPLATE_DIR = os.path.abspath('GrandPyBotApp/templates')
 STATIC_DIR = os.path.abspath('GrandPyBotApp/static')
-KEY_MAPS_PLACES = "API_KEY_PLACES"
+KEY_MAPS_PLACES = "YOUR_KEY_API"
 
